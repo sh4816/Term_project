418d3e665391cc4c6653dd8aa10633fd60303d9a
sh4816 <hpy469@naver.com>
2021-11-21 04:52:45 +0900

    211121_0452
    1. 배경이미지 변경

--- Map_Background.py
+++ Map_Background.py
@@ -12,17 +12,23 @@ class BG:
         self.value = ""
 
         self.image = load_image('BG.png')
-        self.image1 = load_image('BG.png')
+        self.image1 = load_image('BG1.png')
         self.image2 = load_image('BG2.png')
+        self.image3 = load_image('BG3.png')
+        self.imageB1 = load_image('BGB1.png')
 
     def update(self):
         pass
 
     def draw(self):
-        if self.value == "map1" or self.value == "map2_1" or self.value == "map2_3":
+        if self.value == "map1":
             self.image = self.image1
-        elif self.value == "map2_2":
+        elif self.value == "map2_1":
             self.image = self.image2
+        elif self.value == "map2_2":
+            self.image = self.imageB1
+        elif self.value == "map2_3":
+            self.image = self.image3
 
         # 작동 방식: 800x600 크기의 중심을 왼쪽으로 이동시키면서 화면 왼쪽 바깥으로 잘려나간 부분을 화면 오른쪽에서 다시 그려준다.
         # clib_draw: 렌더링 시작위치(x,y), 이미지에서 가져올 범위(w,h), 피봇(cx,cy), 렌더링 사이즈(w,h) 인거같기도...?
--- mygame.py
+++ mygame.py
@@ -5,6 +5,6 @@ import state_intro
 import state_map2_2
 
 pico2d.open_canvas(800, 600)
-game_framework.run(state_map2_2)    # test때 바로 이동할 맵 정상 작동을 할때에 해당 줄을 주석처리하고 아래 줄을 실행
-#game_framework.run(state_intro)
+#game_framework.run(state_map2_2)    # test때 바로 이동할 맵 정상 작동을 할때에 해당 줄을 주석처리하고 아래 줄을 실행
+game_framework.run(state_intro)
 pico2d.close_canvas()
